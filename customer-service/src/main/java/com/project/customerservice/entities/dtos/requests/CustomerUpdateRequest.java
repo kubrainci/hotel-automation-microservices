ea0924399cf021620abe26786aaf58bff3784503
kubrainci <131158025+kubrainci@users.noreply.github.com>
2024-01-03 22:39:42 +0300

    customerservice-validation

--- customer-service/src/main/java/com/project/customerservice/entities/dtos/requests/CustomerUpdateRequest.java
+++ customer-service/src/main/java/com/project/customerservice/entities/dtos/requests/CustomerUpdateRequest.java
@@ -1,5 +1,9 @@
 package com.project.customerservice.entities.dtos.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +15,23 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CustomerUpdateRequest {
 
+    @NotNull(message = "Doğum tarihiniz boş bırakılamaz.")
     private short  birthYear;
+    @NotBlank(message = "Kullanıcı adınız boş bırakılamaz")
     private String name;
+
+    @NotBlank(message = "Kullanıcı soy isminiz boş bırakılamaz.")
     private String lastName;
+
+    @NotBlank(message = "Kullanıcı isminiz boş bırakılamaz")
+    @Size(min = 3,max = 10,message = "Kullanıcı adı minimum 3 maksimum 10 haneden oluşmalıdır.")
     private String userName;
+
+    @NotBlank(message = "Lütfen Gmail hesabınızı yazın.")
+    @Email(
+            message = "Email gecerli degil.",
+            regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
+    @NotBlank(message = "Lütfen şifre oluşturun.")
     private String password;
 }
