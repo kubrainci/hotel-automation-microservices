43dcf76377aebeecb6d173fb1b586238ef548ccf
kubrainci <131158025+kubrainci@users.noreply.github.com>
2024-01-21 13:24:57 +0300

    refactor

--- rental-service/src/main/java/com/project/rentalservice/controllers/RentalsController.java
+++ rental-service/src/main/java/com/project/rentalservice/controllers/RentalsController.java
@@ -1,11 +1,39 @@
 package com.project.rentalservice.controllers;
 
+import com.project.rentalservice.entities.dtos.requests.RentalUpdateRequest;
+import com.project.rentalservice.entities.dtos.responses.RentalGetResponse;
+import com.project.rentalservice.entities.dtos.responses.RentalUpdateResponse;
+import com.project.rentalservice.services.abstracts.RentalService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/rentals")
 public class RentalsController {
+    private RentalService rentalService;
+
+    @GetMapping("/getAll")
+    public  List<RentalGetResponse> getAll(){
+        return rentalService.getAll();
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(int id){
+        rentalService.delete(id);
+    }
+
+    @PutMapping("/update")
+    public RentalUpdateResponse upDate(@RequestParam int id ,@RequestBody RentalUpdateRequest request ){
+        return rentalService.upDate(id,request);
+    }
+
+   @GetMapping("/getById")
+    public RentalGetResponse getById(int id){
+        return rentalService.getById(id);
+   }
+
+
 }
