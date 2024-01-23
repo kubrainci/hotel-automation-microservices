4be327756b67faf1f17229406493c683c314cc81
kubrainci <131158025+kubrainci@users.noreply.github.com>
2024-01-23 11:17:15 +0300

    rentalService-validation

--- rental-service/src/main/java/com/project/rentalservice/entities/dtos/requests/RentalUpdateRequest.java
+++ rental-service/src/main/java/com/project/rentalservice/entities/dtos/requests/RentalUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.project.rentalservice.entities.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,10 @@ import java.time.LocalDate;
 @Builder
 public class RentalUpdateRequest {
 
+    @NotBlank(message = "Kiralama tarihi boş bırakılamaz.")
     LocalDate rentalDate;
 
+    @NotBlank(message = "İnventory kod boş bırakılamaz.")
     private String inventoryCode;
 
 }
--- rental-service/src/main/java/com/project/rentalservice/entities/dtos/requests/SubmitRentalDto.java
+++ rental-service/src/main/java/com/project/rentalservice/entities/dtos/requests/SubmitRentalDto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +18,11 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class SubmitRentalDto {
 
+    @NotBlank(message = "İnventory kod boş bırakılamaz.")
     private  String inventoryCode;
 
     private int customerId;
 
+    @NotBlank(message = "Kiralamanın son tarihi boş bırakılamaz.")
     private LocalDate endDate;
 }
